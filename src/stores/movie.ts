221fc53d82462daee3f51c9e55962433b0171b9a
Elena Litvinova <ivanovaev90@gmail.com>
2023-05-09 19:22:57 +0100

    03-middlewares

--- src/stores/movie.ts
+++ src/stores/movie.ts
@@ -1,27 +1,32 @@
 import { Movie, GetMovieResponse, Status } from '@/types';
 import { create } from 'zustand';
 import { fetchRequest } from '@/helpers/fetch-request';
-
+import { persist, devtools } from 'zustand/middleware';
+;
 interface MovieStore {
   movie?: Movie;
   status: Status;
   loadMovie: (id: number) => void;
 };
 
-export const useMovie = create<MovieStore>((set, get) => ({
-  movie: undefined,
-  status: 'default',
-  loadMovie: async (id) => {
-    set({ status: 'loading' });
+export const useMovie = create<MovieStore>()(
+  devtools(
+    (set, _get) => ({
+      movie: undefined,
+      status: 'default',
+      loadMovie: async (id) => {
+        set({ status: 'loading' });
 
-    try {
-      const data = await fetchRequest<GetMovieResponse>(`/api/movie?id=${id}`);
-      set({
-        status: 'success',
-        movie: data
-      });
-    } catch (err) {
-      set({ status: 'error' });
-    }
-  },
-}));
+        try {
+          const data = await fetchRequest<GetMovieResponse>(`/api/movie?id=${id}`);
+          set({
+            status: 'success',
+            movie: data
+          });
+        } catch (err) {
+          set({ status: 'error' });
+        }
+      },
+    }),
+    { name: "Movie" }
+  ));
--- src/stores/movies.ts
+++ src/stores/movies.ts
@@ -1,6 +1,7 @@
 import { Status, ShortMovie, GetMoviesResponse, MovieType } from '@/types';
 import { create } from 'zustand';
 import { fetchRequest } from '@/helpers/fetch-request';
+import { persist, devtools } from 'zustand/middleware';
 
 interface MoviesStore {
   status: Status;
@@ -13,37 +14,41 @@ interface MoviesStore {
 
 const LOAD_STEP = 6;
 
-export const useMovies = create<MoviesStore>((set, get) => ({
-  movies: [],
-  loadedMovies: 0,
-  total: 0,
-  status: 'default',
-  loadMovies: async (MovieType, reload) => {
-    if (reload) {
-      set({
+export const useMovies = create<MoviesStore>()(
+  devtools(
+      (set, get) => ({
         movies: [],
         loadedMovies: 0,
         total: 0,
-        status: 'default'
-      })
-    }
+        status: 'default',
+        loadMovies: async (MovieType, reload) => {
+          if (reload) {
+            set({
+              movies: [],
+              loadedMovies: 0,
+              total: 0,
+              status: 'default'
+            })
+          }
 
-    const { loadedMovies, movies } =  get();
-    set({ status: 'loading' });
+          const { loadedMovies, movies } =  get();
+          set({ status: 'loading' });
 
-    try {
-      const filterStr = MovieType ? `&filterType=${MovieType}` : '';
-      const data = await fetchRequest<GetMoviesResponse>(`/api/movies?start=${loadedMovies}&end=${loadedMovies + LOAD_STEP}${filterStr}`);
+          try {
+            const filterStr = MovieType ? `&filterType=${MovieType}` : '';
+            const data = await fetchRequest<GetMoviesResponse>(`/api/movies?start=${loadedMovies}&end=${loadedMovies + LOAD_STEP}${filterStr}`);
 
-      set({
-        status: 'success',
-        loadedMovies: Math.min(data.total, loadedMovies + LOAD_STEP),
-        movies: [...movies, ...data.movies],
-        total: data.total,
-      });
-    } catch (err) {
-      set({ status: 'error' });
-    }
-  },
-  isAllMoviesLoaded: () => get().loadedMovies === get().total
-}));
+            set({
+              status: 'success',
+              loadedMovies: Math.min(data.total, loadedMovies + LOAD_STEP),
+              movies: [...movies, ...data.movies],
+              total: data.total,
+            });
+          } catch (err) {
+            set({ status: 'error' });
+          }
+        },
+      isAllMoviesLoaded: () => get().loadedMovies === get().total
+    }),
+    {name: 'Movies'}
+  ));
